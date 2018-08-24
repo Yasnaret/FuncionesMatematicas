770398988663e92a335731342d61f1f7db5c33cc
yasnaret <yasnaret.soto@gmail.com>
2018-08-24 00:19:41 -0300

    last commit

---
+++ ejercicio.js
@@ -0,0 +1,10 @@
+
+const sum=  function(number1,number2){
+    return (number1 + number2) ;
+}
+
+const multiply = function(number3, number4){
+    return (number3 * number4) ;
+}
+
+document.write("El resultado de 36325 *(9824 + 777) es igual a = " + multiply(36325,(sum(9824,777))));
